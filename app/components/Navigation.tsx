5bf5bbebc059c066bfd2140221e109d6b4b18882
hi-rachel <rachel.uiux@gmail.com>
2024-05-20 00:29:05 +0900

    feat: 메인 경로 추가

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,7 +5,9 @@ const Navigation = () => {
     <div className="flex justify-betweens gap-14 text-lg font-bold mb-2">
       <div>Spot Logo</div>
       <div className="flex gap-6">
-        <div>주소, 위도 경도 찾기</div>
+        <Link href={"https://lat-long.vercel.app/"}>
+          <div>주소, 위도 경도 찾기</div>
+        </Link>
         <div>
           <Link href={"https://www.slam-talk.site/map"} target="_blank">
             농구장 지도
